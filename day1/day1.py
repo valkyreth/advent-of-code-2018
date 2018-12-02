de44732bf100279740ea9c01e6c240a0e22db312
Meraj Ahmad <zxanfly@gmail.com>
2018-12-02 20:34:56 +0530

    'intital'

---
+++ day1/day1.py
@@ -0,0 +1,22 @@
+from itertools import cycle
+
+with open('day1.txt', 'r') as file:
+    freqs = [int(i) for i in file.read().split('\n')]
+    
+# Part 1
+
+total = sum(freqs)
+print(f'Part 1 => {total}')
+
+# Part 2
+
+res, i = 0, 0
+dup = set()
+
+for i in cycle(freqs):
+    res += i
+    if res in dup:
+        break
+    dup.add(res)
+
+print(f'Part 2 => {res}')
---
+++ day2/day2.py
@@ -0,0 +1,40 @@
+with open('day2.txt', 'r') as file:
+    lines = file.read().split('\n')
+
+# Part 1
+
+double, triple = 0, 0
+for line in lines:
+    d_check, t_check = False, False
+    for letter in set(line):
+        if not d_check and line.count(letter) == 2:
+            double += 1
+            d_check = True
+        elif not t_check and line.count(letter) == 3:
+            triple += 1
+            t_check = True
+        if d_check and t_check:
+            break
+
+checksum = double * triple
+print(f'Part 1 => {checksum}')
+
+# Part 2
+
+check = False
+for x in range(len(lines)):
+    for y in range(x + 1, len(lines)):
+        diff = 0
+        ans = ''
+        for i in range(len(lines[x])):
+            if lines[x][i] != lines[y][i]:
+                diff += 1
+            else:
+                ans += lines[x][i]
+        if diff == 1:
+            check = True
+            break
+    if check:
+        break
+
+print(f'Part 2 => {ans}')
