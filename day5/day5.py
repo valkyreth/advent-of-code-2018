1188ac84aee0497afd155f29e5ab2981663e64b3
Meraj Ahmad <zxanfly@gmail.com>
2018-12-05 20:35:50 +0530

    day5

--- day5/day5.py
+++ day5/day5.py
@@ -9,15 +9,15 @@ polymer_copy = polymer
 
 i = 0
 while i < len(polymer) - 1:
-    delete = False
+    reaction = False
     if (polymer[i].lower() == polymer[i + 1].lower()) and (
         (polymer[i].islower() and polymer[i + 1].isupper()) or
         (polymer[i].isupper() and polymer[i + 1].islower())):
         polymer = polymer.replace(polymer[i:i + 2], '')
-        delete = True
+        reaction = True
         if i > 0:
             i -= 1
-    if not delete:
+    if not reaction:
         i += 1
 
 shortest = len(polymer)
@@ -28,15 +28,15 @@ for unit in set(polymer_copy.lower()):
     temp = polymer_copy.replace(unit, '').replace(unit.upper(), '')
     i = 0
     while i < len(temp) - 1:
-        delete = False
+        reaction = False
         if (temp[i].lower() == temp[i + 1].lower()) and (
             (temp[i].islower() and temp[i + 1].isupper()) or
             (temp[i].isupper() and temp[i + 1].islower())):
             temp = temp.replace(temp[i:i + 2], '')
-            delete = True
+            reaction = True
             if i > 0:
                 i -= 1
-        if not delete:
+        if not reaction:
             i += 1
     if len(temp) < shortest:
         shortest = len(temp)
